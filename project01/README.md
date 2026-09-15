c3c6999764e42d5771a6e876bc6010c2e29d4d50
dlkasemire <dklinda6@gmail.com>
2026-09-15 18:07:11 -0400

    Add starter functions for parse_line and read_file

--- project01/README.md
+++ project01/README.md
@@ -1,19 +1,54 @@
 # Introduction
-Description of the project
+This project parses a Variant Call Format (VCF) file from ClinVar.
+The goal of the project is to identify rare genetic variants with AF_EXAC less than 0.0001, 
+and tally the disease (CLNDN) associated with them across the dataset.
+The program reads the clinvar_20190923_short.vcf file line per line.
+It then extracts the required fields in the INFO column for each variant.
+It then checks if its rare and builds a tally of disease occurrences among the rare variants.
+The final tally is then printed to the console.
 
 # Pseudocode
-Put pseudocode in this box:
+Function parse_line(line)
+    split line into columns by tab
+    get the last column: info columns
+    split info by ; into key-value pairs
+    build a dictionary of all key=value pairs
+    IF "AF_EXAC" is not in dictionary:
+        RETURN empty list
+    IF AF_EXAC value >= 0.0001
+        RETURN empty list
+    split CLNDN value by "|" to get the disease list
+    remove "not_specified" and "not_provided" from list
+    RETURN remaining disease list
+
+
+FUNCTION read_file(filename):
+    build dictionary of counts
+    open file 
+        for every line
+            if the line starts with #
+                skip to next line
+            strip white spaces at end of each line
+            call parse_line on the line, store result as diseases
+            for each disease in diseases:
+                IF disease in counts:
+                    add 1 to its count
+                ELSE:
+                    set its count to 1
+RETURN counts
 
 
 # Successes
-Description of the team's learning points
+Learned how to create pull requests and collaborate on github.
+
 
 # Struggles
 Description of the stumbling blocks the team experienced
 
 # Personal reflections
 ## Group Leader
-Group leader's reflection on the project
+Initially had trouble working with github.
+
 
 ## Other member
 Other member's reflection on the project
--- project01/project01.py
+++ project01/project01.py
@@ -1,16 +1,75 @@
 #!/usr/bin/env python
 from pprint import pprint
 
+# Modify this function signature and fill in the details
+def parse_line(line):
+
+    # tab separated columns
+    columns = line.split("\t")
+
+    # info is in the last column of the line
+    info = columns[-1]
+
+    # info contains key-value pairs separated by ;
+    key_pairs = info.split(";")
+
+    # make a dictionary of all the key-value pairs
+    info_dict = {}
+
+    # for each key-value pair,
+    for pair in key_pairs:
+       split_pair = pair.split("=") # split by the = sign
+
+       # ignore if there is no value
+       if len(split_pair) < 2:
+           continue
+
+       key = split_pair[0] # index location of key
+       value = split_pair[1] # index location of value
+       info_dict[key] = value
+
+    # Skip line if "AF_EXAC" not present
+    if "AF_EXAC" not in info_dict:
+        return []
+
+    af_value = float(info_dict["AF_EXAC"])
+
+    if af_value >= 0.0001:
+        return []
+
+    if "CLNDN" not in info_dict:
+        return []
+
+    diseases = info_dict["CLNDN"].split("|")
+    filtered_diseases = []
+
+    for disease in diseases:
+        if disease != "not_specified" and disease != "not_provided":
+            filtered_diseases.append(disease)
+
+    return filtered_diseases
+
 
 # Modify this function signature and fill in the details
-def parse_line()
-    pass
+def read_file(filename):
+    counts = {}
 
+    with open("clinvar_20190923_short.vcf", "r") as f:
+        for line in f:
+            if line.startswith("#"):
+                continue
 
-# Modify this function signature and fill in the details
-def read_file()
-    pass
+            line = line.strip()
+            diseases = parse_line(line)
 
+            for disease in diseases:
+                if disease in counts:
+                    counts[disease] += 1
+
+                else:
+                    counts[disease] = 1
+
+    return counts
 
 if __name__ == "__main__":
     pprint(read_file("clinvar_20190923_short.vcf"))
